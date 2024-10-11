a34cf296770483ca82610bef3537307d609f7661
Saviour Assandoh <asandohs@gmail.com>
2024-10-11 04:34:23 +0000

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Atype-annotated function concat that takes a string str1
+and a string str2 as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    A function that returns a concatenated string
+    """
+    return f"{str1} {str2}"
