662b0e5a3c164a2c3bc1507556dad943d2d1e364
Saviour Assandoh <asandohs@gmail.com>
2024-10-11 06:09:12 +0000

    task 100 advance

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Annotation
+"""
+
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    A function that returns any type of data or none 
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
